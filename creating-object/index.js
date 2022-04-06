7d5cec69fa361621f25ddf8b2fe040594415b2dd
Rida <ridacodes@gmail.com>
2022-04-06 21:09:47 +0300

    feat(creating-object): functions are Objects

--- creating-object/index.js
+++ creating-object/index.js
@@ -21,3 +21,16 @@ function Circle(radius) {
 
 const another = new Circle(1);
 console.log("~ another", another);
+
+// Functions are Objects
+const Circle1 = new Function(
+  "radius",
+  `
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
+  }`
+);
+
+const circle1 = new Circle1(1);
+console.log("~ circle1", circle1);
