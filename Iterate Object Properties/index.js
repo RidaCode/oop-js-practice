715b6f69656627ed65a79aa05648aa36c4047c0f
Rida <ridacodes@gmail.com>
2022-04-06 22:24:44 +0300

    feat(iterate object properties): get all properties and values in an object using for in loop

--- Iterate Object Properties/index.js
+++ Iterate Object Properties/index.js
@@ -13,3 +13,9 @@ console.log("~ all the properties in the circle");
 for (let key in circle) {
   console.log(key);
 }
+
+// Get all properties and values in an object using for in loop
+console.log("~ all the properties with values in the circle");
+for (let key in circle) {
+  console.log(key, circle[key]);
+}
