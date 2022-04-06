f6739b51a4e164b7ce71b537b6bd045b6c04551c
Rida <ridacodes@gmail.com>
2022-04-06 22:10:00 +0300

    feat(adding removing properties): removing properties
    using delete operator with dot or bracket notation

--- Adding Removing Properties/index.js
+++ Adding Removing Properties/index.js
@@ -13,3 +13,8 @@ console.log("~ circle before adding property \n", circle);
 circle.location = { x: 1 }; //using dot notation
 circle["location"] = { x: 1 }; //using bracket notation
 console.log("~ circle after adding property \n", circle);
+
+// Removing properties
+delete circle.location; // using delete operator with dot notation
+delete circle["location"]; // using delete operator with bracket notation
+console.log("~ circle after deleting property \n", circle);
