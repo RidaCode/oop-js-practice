dc6a9af1686324c1613f99c5198b1a1c87c850d5
Rida <ridacodes@gmail.com>
2022-04-06 21:33:37 +0300

    feat(value vs reference types): value Type showcase

--- Value vs Reference Types/index.js
+++ Value vs Reference Types/index.js
@@ -6,6 +6,12 @@ Symbol;
 undefined;
 null;
 
+// y is a copy from x, and both are independent from each other
+let x = 10;
+let y = x;
+x = 20;
+console.log("x", x, "y", y);
+
 // Reference Types
 Object;
 Function;
