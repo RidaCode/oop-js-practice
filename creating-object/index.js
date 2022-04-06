e352a90eb38619704f35a5a35e87d57f92d40fb6
Rida <ridacodes@gmail.com>
2022-04-06 20:59:06 +0300

    feat(creating-object): creating a constructor function

--- creating-object/index.js
+++ creating-object/index.js
@@ -9,5 +9,15 @@ function createCircle(radius) {
 }
 
 const circle = createCircle(1);
+console.log("~ circle", circle);
 
-circle.draw();
+// Constructor Function
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
+  };
+}
+
+const another = new Circle(1);
+console.log("~ another", another);
